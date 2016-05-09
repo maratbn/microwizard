4eee2c5b8251889365045bc63cdbc1d9b034aba8
Marat Nepomnyashy <maratbn@gmail.com>
2016-05-09 00:33:43 -0700

    Added additional necessary dependency packages 'libffi-devel' and 'openssl-devel' required by the Python environment on the CentOS guest.

--- provisioning/scripts/bootstrap.bash
+++ provisioning/scripts/bootstrap.bash
@@ -5,6 +5,6 @@
 
 yum -y install epel-release
 yum -y update
-yum -y install gcc python-devel python-pip git
+yum -y install gcc python-devel python-pip git libffi-devel openssl-devel
 yes | pip install --upgrade pip
 yes | pip install ansible
